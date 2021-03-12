ffb363c06780f3b27457ec36a62b3ea23382ab82
Low Entropy Body <45514917+sutdorg@users.noreply.github.com>
2021-03-12 15:50:50 +0800

    add vector clock branch and initialize files

--- pkg/ch/consistenthashing.go
+++ pkg/ch/consistenthashing.go
@@ -1,1 +1,8 @@
 package ch
+
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
--- pkg/ch/consistenthashing_test.go
+++ pkg/ch/consistenthashing_test.go
@@ -1,1 +1,14 @@
 package ch
+
+import "testing"
+
+func TestFib(t *testing.T) {
+	var (
+		in       = 7
+		expected = 13
+	)
+	actual := Fib(in)
+	if actual != expected {
+		t.Errorf("Fib(%d) = %d; expected %d", in, actual, expected)
+	}
+}
