e18066da31805e1f80cf3fcccd5293ebb6670f35
Low Entropy Body <45514917+sutdorg@users.noreply.github.com>
2021-03-12 18:57:31 +0800

    update proto

--- pkg/dto/dto.pb.go
+++ pkg/dto/dto.pb.go
@@ -214,6 +214,53 @@ func (x *GetResponse) GetObject() []byte {
 	return nil
 }
 
+type VectorClock struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Vclock map[int64]int64 `protobuf:"bytes,1,rep,name=vclock,proto3" json:"vclock,omitempty" protobuf_key:"varint,1,opt,name=key,proto3" protobuf_val:"varint,2,opt,name=value,proto3"`
+}
+
+func (x *VectorClock) Reset() {
+	*x = VectorClock{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pkg_dto_dto_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VectorClock) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VectorClock) ProtoMessage() {}
+
+func (x *VectorClock) ProtoReflect() protoreflect.Message {
+	mi := &file_pkg_dto_dto_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VectorClock.ProtoReflect.Descriptor instead.
+func (*VectorClock) Descriptor() ([]byte, []int) {
+	return file_pkg_dto_dto_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *VectorClock) GetVclock() map[int64]int64 {
+	if x != nil {
+		return x.Vclock
+	}
+	return nil
+}
+
 var File_pkg_dto_dto_proto protoreflect.FileDescriptor
 
 var file_pkg_dto_dto_proto_rawDesc = []byte{
@@ -227,7 +274,15 @@ var file_pkg_dto_dto_proto_rawDesc = []byte{
 	0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x22,
 	0x25, 0x0a, 0x0b, 0x47, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16,
 	0x0a, 0x06, 0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x06,
-	0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x32, 0x67, 0x0a, 0x0d, 0x4b, 0x65, 0x79, 0x56, 0x61, 0x6c,
+	0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x22, 0x7e, 0x0a, 0x0b, 0x56, 0x65, 0x63, 0x74, 0x6f, 0x72,
+	0x43, 0x6c, 0x6f, 0x63, 0x6b, 0x12, 0x34, 0x0a, 0x06, 0x76, 0x63, 0x6c, 0x6f, 0x63, 0x6b, 0x18,
+	0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x1c, 0x2e, 0x64, 0x74, 0x6f, 0x2e, 0x56, 0x65, 0x63, 0x74,
+	0x6f, 0x72, 0x43, 0x6c, 0x6f, 0x63, 0x6b, 0x2e, 0x56, 0x63, 0x6c, 0x6f, 0x63, 0x6b, 0x45, 0x6e,
+	0x74, 0x72, 0x79, 0x52, 0x06, 0x76, 0x63, 0x6c, 0x6f, 0x63, 0x6b, 0x1a, 0x39, 0x0a, 0x0b, 0x56,
+	0x63, 0x6c, 0x6f, 0x63, 0x6b, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65,
+	0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05,
+	0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x05, 0x76, 0x61, 0x6c,
+	0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x32, 0x67, 0x0a, 0x0d, 0x4b, 0x65, 0x79, 0x56, 0x61, 0x6c,
 	0x75, 0x65, 0x53, 0x74, 0x6f, 0x72, 0x65, 0x12, 0x2a, 0x0a, 0x03, 0x50, 0x75, 0x74, 0x12, 0x0f,
 	0x2e, 0x64, 0x74, 0x6f, 0x2e, 0x50, 0x75, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
 	0x10, 0x2e, 0x64, 0x74, 0x6f, 0x2e, 0x50, 0x75, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
@@ -252,23 +307,26 @@ func file_pkg_dto_dto_proto_rawDescGZIP() []byte {
 	return file_pkg_dto_dto_proto_rawDescData
 }
 
-var file_pkg_dto_dto_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_pkg_dto_dto_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_pkg_dto_dto_proto_goTypes = []interface{}{
 	(*PutRequest)(nil),  // 0: dto.PutRequest
 	(*PutResponse)(nil), // 1: dto.PutResponse
 	(*GetRequest)(nil),  // 2: dto.GetRequest
 	(*GetResponse)(nil), // 3: dto.GetResponse
+	(*VectorClock)(nil), // 4: dto.VectorClock
+	nil,                 // 5: dto.VectorClock.VclockEntry
 }
 var file_pkg_dto_dto_proto_depIdxs = []int32{
-	0, // 0: dto.KeyValueStore.Put:input_type -> dto.PutRequest
-	2, // 1: dto.KeyValueStore.Get:input_type -> dto.GetRequest
-	1, // 2: dto.KeyValueStore.Put:output_type -> dto.PutResponse
-	3, // 3: dto.KeyValueStore.Get:output_type -> dto.GetResponse
-	2, // [2:4] is the sub-list for method output_type
-	0, // [0:2] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	5, // 0: dto.VectorClock.vclock:type_name -> dto.VectorClock.VclockEntry
+	0, // 1: dto.KeyValueStore.Put:input_type -> dto.PutRequest
+	2, // 2: dto.KeyValueStore.Get:input_type -> dto.GetRequest
+	1, // 3: dto.KeyValueStore.Put:output_type -> dto.PutResponse
+	3, // 4: dto.KeyValueStore.Get:output_type -> dto.GetResponse
+	3, // [3:5] is the sub-list for method output_type
+	1, // [1:3] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_pkg_dto_dto_proto_init() }
@@ -325,6 +383,18 @@ func file_pkg_dto_dto_proto_init() {
 				return nil
 			}
 		}
+		file_pkg_dto_dto_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VectorClock); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -332,7 +402,7 @@ func file_pkg_dto_dto_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_pkg_dto_dto_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
