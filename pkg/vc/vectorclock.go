442d9532235bd4ccfd614094d282a9df5f85c2ec
Low Entropy Body <45514917+sutdorg@users.noreply.github.com>
2021-03-12 16:59:01 +0800

    update functions: NewVectorClock, MergeClock, Advance

--- pkg/vc/vectorclock.go
+++ pkg/vc/vectorclock.go
@@ -6,3 +6,51 @@ func Fib(n int) int {
 	}
 	return Fib(n-1) + Fib(n-2)
 }
+
+// NewVectorClock
+// MergeClock
+// Advance
+// ToDTO
+// FromDTO
+
+type vclock map[int]int // vector clock type
+
+func NewVectorClock(machineID int) vclock {
+	return vclock{machineID: 0}
+}
+
+func (selfclock vclock) MergeClock(machineID int, otherclock vclock) bool {
+	// MAP has no order
+	result := false
+	numOfEqual := 0
+	// merge first
+	for k, v := range otherclock {
+		if val, ok := selfclock[k]; ok {
+			// If the local clock is after (in line with the vector comparison)
+			// the message clock, then a (potential) causality violation is flagged
+			if val > v {
+				result = true
+			} else if val == v {
+				// After means more than or equal, >=,
+				// but CANT be the case where all elements are equal
+				numOfEqual++
+			} else {
+				selfclock[k] = v
+			}
+		} else {
+			// this key from otherclock is not in selfclock
+			selfclock[k] = v
+		}
+	}
+	// advance clock now
+	selfclock.Advance(machineID)
+	// need to return causality violation
+	if numOfEqual == len(selfclock) {
+		result = true
+	}
+	return result
+}
+
+func (selfclock vclock) Advance(machineID int) {
+	selfclock[machineID]++
+}
--- pkg/vc/vectorclock_test.go
+++ pkg/vc/vectorclock_test.go
@@ -1,6 +1,9 @@
 package vc
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestFib(t *testing.T) {
 	var (
@@ -10,5 +13,56 @@ func TestFib(t *testing.T) {
 	actual := Fib(in)
 	if actual != expected {
 		t.Errorf("Fib(%d) = %d; expected %d", in, actual, expected)
+	}
+}
+
+func TestNewVectorClock(t *testing.T) {
+	var (
+		in       = 7
+		expected = vclock{7: 0}
+	)
+	actual := NewVectorClock(in)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("NewVectorClock(%v) = %v; expected %v", in, actual, expected)
+	}
+}
+
+func TestVclock_MergeClock(t *testing.T) {
+	// prepare two vector clocks
+	c1 := NewVectorClock(1)
+	c2 := NewVectorClock(2)
+	c1.Advance(1)
+	c1.Advance(1)
+	c1.Advance(1)
+	c2.Advance(2)
+
+	expected := vclock{1: 4, 2: 1}
+
+	c1.MergeClock(1, c2)
+	if !reflect.DeepEqual(c1, expected) {
+		t.Errorf("MergeClock(%v) = %v; expected %v", c2, c1, expected)
+	}
+}
+
+func TestVclock_MergeClock2(t *testing.T) {
+	// Test detection of causality violation
+	// prepare two vector clocks
+	c1 := vclock{1: 3, 2: 5} // local clock
+	c2 := vclock{1: 2, 2: 5, 3: 4}
+	expected := true
+
+	actual := c1.MergeClock(1, c2)
+	if actual != expected {
+		t.Errorf("MergeClock(%v) = %v; expected %v", c2, actual, expected)
+	}
+}
+
+func TestVclock_Advance(t *testing.T) {
+	c1 := vclock{1: 3, 2: 5}
+
+	expected := vclock{1: 4, 2: 5}
+	c1.Advance(1)
+	if !reflect.DeepEqual(c1, expected) {
+		t.Errorf("MergeClock(%v) = %v; expected %v", c1, c1, expected)
 	}
 }
